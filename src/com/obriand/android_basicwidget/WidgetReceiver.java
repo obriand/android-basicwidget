4e6b162b89548e30983d05533ddbbe71756fa089
Olivier Briand <olivier.briand@orange.com>
2013-01-21 17:01:02 +0100

    Improve the code

--- src/com/obriand/android_basicwidget/WidgetReceiver.java
+++ src/com/obriand/android_basicwidget/WidgetReceiver.java
@@ -18,47 +18,45 @@ public class WidgetReceiver extends AppWidgetProvider
 	@Override
 	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) 
 	{
-		final int N = appWidgetIds.length;
-	
+		Toast.makeText(context, "onUpdate", Toast.LENGTH_SHORT).show(); 
 		// Pour chaque AppWidget MonWidgetDeveloppez (n'oubliez pas qu'on peut en ajouter tant qu'on veut), on les met a jour :
-		for (int i = 0; i < N; i++) 
-		{
-			int appWidgetId = appWidgetIds[i];
+		for (int appWidgetId : appWidgetIds) {
 			updateAppWidget(context, appWidgetManager, appWidgetId);
 		}
 	}
-	
-	
-	
 
 	// Cette methode est entierement libre, a vous de la modifier comme bon vous semble. Voici toutefois une base minimaliste
-	static void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId) 
+	public void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId) 
 	{
-		RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_main); // On recupere les Views de notre layout
-		views.setTextViewText(R.id.content, "Hello Developpez !"); // On peut agir sur ces vues
-		appWidgetManager.updateAppWidget(appWidgetId, views); // On met ensuite a jour l'affichage du widget
+		Toast.makeText(context, "onUpdate", Toast.LENGTH_SHORT).show(); 
 		
-		// On prépare un intent a lancer lors d'un clic
+		RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_main); // On recupere les Views de notre layout
+		remoteViews.setTextViewText(R.id.widget_title_tv, "Hello Developpez !"); // On peut agir sur ces vues
+		appWidgetManager.updateAppWidget(appWidgetId, remoteViews); // On met ensuite a jour l'affichage du widget
+		
+		// On prepare un intent a lancer lors d'un clic
 		Intent intent = new Intent(context, WidgetReceiver.class);
 		intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
 		intent.setAction(ACTION_LANCER_APPLICATION); // Je cree ici ma propre action
 
 		// On lie l'intent a l'action
 		PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
-		views.setOnClickPendingIntent(R.id.text_layout, pendingIntent); // L'id de la view qui reagira au clic sur le widget.
-		appWidgetManager.updateAppWidget(appWidgetId, views);
+		remoteViews.setOnClickPendingIntent(R.id.widget_content_tv, pendingIntent); // L'id de la view qui reagira au clic sur le widget.
+		appWidgetManager.updateAppWidget(appWidgetId, remoteViews);
 	}
 	
 	@Override
 	public void onReceive(Context context, Intent intent) 
 	{
 	    super.onReceive(context, intent);
-	    Toast.makeText(context, "onReceive", Toast.LENGTH_LONG).show(); 
+	    Toast.makeText(context, "onReceive", Toast.LENGTH_SHORT).show(); 
 	    if (intent.getAction().equals(ACTION_LANCER_APPLICATION))
 	    {
+	    	Toast.makeText(context, "ACTION_LANCER_APPLICATION", Toast.LENGTH_SHORT).show(); 
 	        lancerActivityPrincipale(context);
 	    }
 	}
+	
 
 	protected void lancerActivityPrincipale(Context context) 
 	{
